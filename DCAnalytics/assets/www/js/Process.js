d34fbb1dad067b206baf18eb0bef41f3e6afbb1c
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-02-09 15:02:51 -0800

    push

--- DCAnalytics/assets/www/js/Process.js
+++ DCAnalytics/assets/www/js/Process.js
@@ -1,10 +1,34 @@
+// Rishi Kapadia & Robert 
+// 2-9-13
 
-Function Mangitude(foodGroup){
-	for (var i=0;i<foodGroup.length;i++)
-	{ 
-		document.write(cars[i] + "<br>");
+Function Magnitude(foodGroup, prefs){
+
+	var result == {};
+	var site_meal_total = 0;
+
+	for (var site in foodgroup.keys)
+	{
+		result[site] = {};
+		for (var mealTime in site.keys)
+		{
+			result[site][mealTime] = {};
+			for (var key in prefs.keys)
+			{
+				result[site][mealTime]= prefs[key] * foodGroup[site][mealTime][key];
+					//
+			}
+		}
 	}
 
 
+	return result;
 }
 
+/* OUTPUT
+ *  result = {site1: {'Breakfast': #, 'Lunch': #, 'Dinner': #},
+ *			  site2: {...},
+ *			  site3: {...}}
+ */
+
+
+// obj.keys  //dictionary
