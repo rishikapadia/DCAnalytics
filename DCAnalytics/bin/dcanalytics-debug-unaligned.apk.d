022b1f271777bcce48f6949577d143620bf39697
Alexander Chu <arctangent1759@ubuntu.(none)>
2013-02-09 15:22:09 -0800

    Scraper.js

---
+++ DCAnalytics/bin/dcanalytics-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/arctangent1759/Documents/Projects/Ongoing/DCAnalytics/DCAnalytics/bin/dcanalytics-debug-unaligned.apk : \
+/home/arctangent1759/Documents/Projects/Ongoing/DCAnalytics/DCAnalytics/bin/dcanalytics.ap_ \
+/home/arctangent1759/Documents/Projects/Ongoing/DCAnalytics/DCAnalytics/bin/classes.dex \
+/home/arctangent1759/Documents/Projects/Ongoing/DCAnalytics/DCAnalytics/libs/cordova-2.2.0.jar \
