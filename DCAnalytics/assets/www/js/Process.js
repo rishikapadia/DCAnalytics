204fab9ea97b0e3ded56590f6f9e184b688cd354
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-02-09 15:43:42 -0800

    comments

--- DCAnalytics/assets/www/js/Process.js
+++ DCAnalytics/assets/www/js/Process.js
@@ -3,35 +3,36 @@
 
 Function Magnitude(foodGroup, prefs){
 
-	var result == {};
-	var site_meal_total = 0;
+	var result == {};  
+		// dictionary of dictionaries to contain ratings for each site/mealtime
 
-	for (var site in foodgroup.keys)//for every site
+	for (var site in foodgroup.keys)
 	{
 		var items = 0;
 		result[site] = {};
-		for (var mealTime in site.keys)
+		
+		for (var mealTime in site.keys)    //Breakfast, Lunch, and Dinner
 		{
-			result[site][mealTime] = 0;
+			result[site][mealTime] = 0;    //total rating so far
 			for (var key in prefs.keys)
 			{
-				if(key in foodgroup[site][mealTime]){}
+				if(key in foodgroup[site][mealTime]
+				{
 					result[site][mealTime] += prefs[key];
-					items++;
-					//
+					items++;            //number of items tally
 				}
 			}
 		}
 		result[site][mealTime] /= resultMath.sqrt(items)
+			//dividing by vector magnitude
 	}
 	return result;
 }
+
+
 
 /* OUTPUT
  *  result = {site1: {'Breakfast': #, 'Lunch': #, 'Dinner': #},
  *			  site2: {...},
  *			  site3: {...}}
  */
-
-
-// obj.keys  //dictionary
