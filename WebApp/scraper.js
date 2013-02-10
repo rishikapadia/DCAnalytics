814d8f9db35db635a26435d5c1d37859ead72d91
Alexander Chu <arctangent1759@ubuntu.(none)>
2013-02-09 16:33:06 -0800

    can grab webpage

--- WebApp/scraper.js
+++ WebApp/scraper.js
@@ -1,11 +1,13 @@
-function getRawHtml(url,onLoad,onFail=function(){}){
+var DCURL="http://services.housing.berkeley.edu/FoodPro/dining/static/todaysentrees.asp"
+
+function getRawHtml(url,onLoad,onFail=function(data){}){
 	var data=null;
 	$.ajax({
 		url: url,
 		type: 'GET',
 		success: function(res) {
 			data = res.responseText;
-			onLoad(toLineArray(data));
+			onLoad(data);
 		},
 		failure: function(res){
 			data="FAILURE";
@@ -13,6 +15,13 @@ function getRawHtml(url,onLoad,onFail=function(){}){
 		}
 	});
 }
+
+function getLineArrayHtml(url, onLoad, onFail = function(data){}){
+	getRawHtml(url,function(data){onLoad(toLineArray(data))},onFail);
+}
+
 function toLineArray(data){
 	return data.split("\n");
 }
+
+getLineArrayHtml(DCURL,parser);
