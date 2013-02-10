b368b59aad45932a6653634da7b524a6cf887a78
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-02-09 18:28:09 -0800

    added string format and empty function

--- WebApp/prettify.js
+++ WebApp/prettify.js
@@ -1,6 +1,26 @@
-function generateUserDisplayHTML(mealRating,meal){
 
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+          return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
+
+function generateUserDisplayHTML(mealRating, meal){
+    
 }
 function appendUserDisplayHTML(html){
 	document.getElementByID("userdisplay").html
 }
+
+function generateHTML(color, site, rating){
+
+}
+
