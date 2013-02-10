20e791e649da7652d325d06294dead2792307fe9
Alexander Chu <arctangent1759@ubuntu.(none)>
2013-02-09 18:58:49 -0800

    added template constants

---
+++ WebApp/prettify_constants.js
@@ -0,0 +1,54 @@
+var BLUEGRADIENT="\
+				background: -moz-linear-gradient(top,  #1e5799 0%, #2989d8 50%, #207cca 51%, #7db9e8 100%); /* FF3.6+ */\
+				background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#1e5799), color-stop(50%,#2989d8), color-stop(51%,#207cca), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */\
+				background: -webkit-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* Chrome10+,Safari5.1+ */\
+				background: -o-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* Opera 11.10+ */\
+				background: -ms-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* IE10+ */\
+				background: linear-gradient(to bottom,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* W3C */\
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); /* IE6-9 */\
+				"
+var GOLDGRADIENT="\
+				background: #33ffff; /* Old browsers */\
+				background: -moz-linear-gradient(top,  #33ffff 0%, #d8782a 0%, #ca6f20 51%, #e8ac7e 100%); /* FF3.6+ */\
+				background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#33ffff), color-stop(0%,#d8782a), color-stop(51%,#ca6f20), color-stop(100%,#e8ac7e)); /* Chrome,Safari4+ */\
+				background: -webkit-linear-gradient(top,  #33ffff 0%,#d8782a 0%,#ca6f20 51%,#e8ac7e 100%); /* Chrome10+,Safari5.1+ */\
+				background: -o-linear-gradient(top,  #33ffff 0%,#d8782a 0%,#ca6f20 51%,#e8ac7e 100%); /* Opera 11.10+ */\
+				background: -ms-linear-gradient(top,  #33ffff 0%,#d8782a 0%,#ca6f20 51%,#e8ac7e 100%); /* IE10+ */\
+				background: linear-gradient(to bottom,  #33ffff 0%,#d8782a 0%,#ca6f20 51%,#e8ac7e 100%); /* W3C */\
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#33ffff', endColorstr='#e8ac7e',GradientType=0 ); /* IE6-9 */\
+				"
+var GREENGRADIENT="\
+				background: #5533ff; /* Old browsers */\
+				background: -moz-linear-gradient(top,  #5533ff 0%, #6dd82a 0%, #5eca20 51%, #a8e87e 100%); /* FF3.6+ */\
+				background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#5533ff), color-stop(0%,#6dd82a), color-stop(51%,#5eca20), color-stop(100%,#a8e87e)); /* Chrome,Safari4+ */\
+				background: -webkit-linear-gradient(top,  #5533ff 0%,#6dd82a 0%,#5eca20 51%,#a8e87e 100%); /* Chrome10+,Safari5.1+ */\
+				background: -o-linear-gradient(top,  #5533ff 0%,#6dd82a 0%,#5eca20 51%,#a8e87e 100%); /* Opera 11.10+ */\
+				background: -ms-linear-gradient(top,  #5533ff 0%,#6dd82a 0%,#5eca20 51%,#a8e87e 100%); /* IE10+ */\
+				background: linear-gradient(to bottom,  #5533ff 0%,#6dd82a 0%,#5eca20 51%,#a8e87e 100%); /* W3C */\
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#5533ff', endColorstr='#a8e87e',GradientType=0 ); /* IE6-9 */\
+				"
+var REDGRADIENT="\
+				background: #00dc9a; /* Old browsers */\
+				background: -moz-linear-gradient(top,  #00dc9a 0%, #ba1c00 0%, #a81c00 51%, #ff2503 100%); /* FF3.6+ */\
+				background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#00dc9a), color-stop(0%,#ba1c00), color-stop(51%,#a81c00), color-stop(100%,#ff2503)); /* Chrome,Safari4+ */\
+				background: -webkit-linear-gradient(top,  #00dc9a 0%,#ba1c00 0%,#a81c00 51%,#ff2503 100%); /* Chrome10+,Safari5.1+ */\
+				background: -o-linear-gradient(top,  #00dc9a 0%,#ba1c00 0%,#a81c00 51%,#ff2503 100%); /* Opera 11.10+ */\
+				background: -ms-linear-gradient(top,  #00dc9a 0%,#ba1c00 0%,#a81c00 51%,#ff2503 100%); /* IE10+ */\
+				background: linear-gradient(to bottom,  #00dc9a 0%,#ba1c00 0%,#a81c00 51%,#ff2503 100%); /* W3C */\
+				filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00dc9a', endColorstr='#ff2503',GradientType=0 ); /* IE6-9 */\
+				"
+var DIVTEMPLATE='\
+				<div style="{2}width:50%;height:5em;color:black;border:1px solid gray; border-radius:15px; background-color:#ff0000;position:relative; vertical-align:middle;">\
+				<div style="width:25%;float:left;">\
+				<h1>\
+				&nbsp {0}\
+				</h1>\
+				</div>\
+				<div style="width:15%; float:right;">\
+				<table>\
+				<tr><td>Score</td></tr>\
+				<tr><td bgcolor="white" align="center" height="40px">{1}</td></tr>\
+				</table>\
+				</div>\
+				</div>\
+				'
